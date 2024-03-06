cfa64970ea47387a4572207b780e7455d44f2d1c
Diato <85032137+Diatomita@users.noreply.github.com>
2024-03-06 20:13:13 -0300

    funcoes separadas do arquivo main

--- bin/champions.dart
+++ bin/champions.dart
@@ -1,9 +1,43 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:nyxx/nyxx.dart';
 import 'dart:math';
+import 'teamchaos.dart';
 
-String pathArquivoChamps = 'champions.txt';
+// Atualiza a lista de personagens
+Future<String> botUpdate() async {
+  // Consulta a API e retorna o JSON com os bonecos
+  logTool('Buscando os dados...');
+  var url = Uri.parse(apiURL);
+  
+  var response = await http.get(url, headers: apiHeaders);
+  logTool('Dados encontrados.');
 
-void main() {
+  if (response.statusCode == 200) {
+    // Decodifica o JSON pra um mapa
+    Map<String, dynamic> championsMap = json.decode(response.body);
+
+    // Extrai os nomes dos bonecos
+    List<String> champions = championsMap.keys.toList();
+
+    // Grava os nomes em champions.txt
+    File championsFile = File('champions.txt');
+    championsFile.writeAsStringSync(champions.join('\n'));
+    
+    String successMsg = 'Os dados foram atualizados com sucesso!';
+    logTool(successMsg);
+    return successMsg;
+  } else {
+    String errorMsg = 'Erro ao atualizar os dados!';
+    logTool(errorMsg);
+    return errorMsg;
+  }
+}
+
+// Monta o time aleatório
+Future<String> botRoleta() async {
   // Le o arquivo e adiciona cada linha em uma lista
   List<String> champList = [];
   File(pathArquivoChamps).readAsLinesSync().forEach((champ) {
@@ -19,5 +53,30 @@ void main() {
     champSelected.add(champList[champAleatorio]);
   }
 
-  print('Nomes selecionados:\n${champSelected.join('\n')}');
+  logTool(champSelected.join('-'));
+  String resultado = 'Time formado:\n\n${champSelected.join('\n')}';
+  return resultado;
+}
+
+// KHAZIX
+Future<String> botBarata() async {
+  String msg = 'KHAZIX :cockroach::cockroach::cockroach:';
+  logTool(msg);
+  return msg;
+}
+
+// POPLES
+Future<String> botPoples() async {
+    String msg = 'poples';
+    logTool(msg);
+    return msg;
+}
+
+void commandInterpreter(event, botUser, String command, Future<String> Function() func) async {
+  if (event.mentions.contains(botUser) && event.message.content.toLowerCase().contains(command)) {
+    await event.message.channel.sendMessage(MessageBuilder(
+        content: await func(),
+        replyId: event.message.id,
+    ));
+  }
 }
