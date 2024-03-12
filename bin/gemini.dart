53f2a895b7a4b63f370f0817002eb7418b86e1dc
DIATOMITA <brenofelipelira@live.com>
2024-03-12 00:13:23 -0300

    gemini.dart changes

--- bin/gemini.dart
+++ bin/gemini.dart
@@ -5,13 +5,18 @@ import 'package:google_generative_ai/google_generative_ai.dart';
 final tokenGeminiPath = 'tokenGemini';
 final tokenGemini = File(tokenGeminiPath).readAsStringSync();
 final modelGemini = GenerativeModel(model: 'gemini-pro', apiKey:tokenGemini);
-void main() async {
-  while (true) {
-    stdout.write('Prompt: ');
-    String prompt = stdin.readLineSync()!;
 
-    var content = [Content.text(prompt)];
-    var response = await modelGemini.generateContent(content);
-    print('Output: ${response.text}');
-  }
+Future<String> gemini(String prompt) async {
+
+  var content = [Content.text(prompt)];
+  var response = await modelGemini.generateContent(content);
+  return response.text!;
 }
+
+/*
+void main() async {
+  var prompt = 'eu gosto de futebol';
+  var output = await gemini(prompt);
+  print(output);
+}
+*/
